a1157159bee97e50a4b46f469e19f45c674997e3
Aybike Gucuk <aybike.gck@gmail.com>
2026-05-26 15:56:19 +0300

    fix: indentation duzeltildi

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -27,8 +27,7 @@ def driver():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1280,900')
-
-options.binary_location = "/usr/bin/chromium"
+    options.binary_location = "/usr/bin/chromium""
     try:
         from webdriver_manager.chrome import ChromeDriverManager
         service = Service(ChromeDriverManager().install())
